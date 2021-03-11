fe31e8b6240c029560e03e530ac4bc6cb38142bc
fubukai <karn_muan@hotmail.com>
2021-03-11 14:55:18 +0700

    Add files via upload

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+
 from elearning import views
 
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^admin/', admin.site.urls),
     url('home/', views.home, name='home'),
+    url('select/', views.select, name='select'),
     url('menu/', views.menu, name='menu'),
     url('feedback/', views.feedback, name='feedback'),
     url('ihub_test/', views.ihub_test, name='ihub_test'),
@@ -30,7 +32,6 @@ urlpatterns = [
     url('virtualclass/', views.virtualclass, name='virtualclass'),
     url('ihub_test_summary/', views.ihub_test_summary, name='ihub_test_summary'),
     path('Course_main/<int:PK_Course_D>/', views.Course_main, name='Course_main'),
-    path('eva_chart/<int:PK_Course_D>/',views.eva_chart,name='eva_chart'),#
     path('ihub_score/<int:Staff_ID>/', views.ihub_score, name='ihub_score'),
     path('VDO/<int:PK_Title>/', views.VDO, name='VDO'),
     path('Pretest/<int:PK_Course_D>/', views.pretest, name='pretest'),
